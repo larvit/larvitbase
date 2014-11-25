b9a961b20a39d469e884ca20233a95ca51a55fdd
Lilleman <lilleman@larvit.se>
2014-11-25 20:20:13 +0100

    Initial commit

---
+++ base.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var path        = require('path'),
+    appPath     = path.dirname(require.main.filename),
+    serverConf  = require(appPath + '/config/server.json'),
+    http        = require('http'),
+    serveStatic = require('serve-static')(serverConf.pubFilePath, {'index': false}),
+    formidable  = require('formidable'),
+    router      = undefined;
+
+createServer(require(appPath + '/config/routes.json'));
+
+// Actually create the server
+function createServer(customRoutes) {
+	router = require('larvitrouter')({'customRoutes': customRoutes, 'debug': serverConf.debug});
+
+	http.createServer(function(request, response) {
+		if (serverConf.debug === true)
+			console.time('Complete request to ' + request.url);
+
+		router.resolve(request, function(err){
+			if (err) {
+				console.error(err);
+				request.controllerName = '404';
+			}
+
+			// We need to parse the request a bit for POST values etc before we hand it over to the controller(s)
+			parseRequest(request, response);
+
+			// Benchmark this whole request + response
+			if (serverConf.debug === true) {
+				response.on('finish', function () {
+					console.timeEnd('Complete request to ' + request.url);
+				});
+			}
+		});
+	}).listen(serverConf.port, '127.0.0.1');
+}
+
+/**
+ * Parse request
+ * Fetch POST data etc
+ */
+function parseRequest(request, response) {
+	if (request.method == 'POST') {
+		var form = new formidable.IncomingForm();
+
+		form.parse(request, function(err, fields, files){
+			if (err) {
+				console.error(err);
+				response.writeHead(500, {'content-type': 'text/plain'});
+				response.end('Internal server error');
+			} else {
+				request.formFields = fields;
+				request.formFiles  = files;
+				executeController(request, response);
+			}
+		});
+	} else {
+		// No parsing needed, just execute the controller
+		executeController(request, response);
+	}
+}
+
+function executeController(request, response) {
+	if (request.staticFilename !== undefined)
+		serveStatic(request, response);
+	else if (request.controllerName !== undefined)
+		require(appPath + '/controllers/' + request.controllerName).run(request, response, router.sendToClient);
+	else
+		require(appPath + '/controllers/404').run(request, response, router.sendToClient);
+}
