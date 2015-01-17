7b3aa78a9c3903675d0d8ffa37e1d93c9b750d26
Lilleman <lilleman@larvit.se>
2015-01-18 00:50:25 +0100

    Updated logging performance checks and more

--- README.md
+++ README.md
@@ -12,7 +12,9 @@ In your application root directory, create a file named server.js with the follo
 
     'use strict';
 
-    var larvitbase = require('larvitbase');
+    var customRoutes = {}; // Follows the same syntax as the routes.json file below
+
+    require('larvitbase')(customRoutes);
 
 You also need to do the configuration below before you have a working system
 
@@ -21,13 +23,14 @@ You also need to do the configuration below before you have a working system
 Two configuration files are required:
 
 * ./config/server.json
-* ./config/router.json
+* ./config/routes.json
 
 #### server.json
 
 Example:
 
     {
+        "host":        "127.0.0.1",
     	"port":        8001,
     	"pubFilePath": "./public"
     }
@@ -56,7 +59,7 @@ The second will match exactly "/mupp" and will load up the controller at ./contr
 
 A controller needs to fill a few criterias.
 
-It must reside in ./controller/<controllerName>.js - for example ./controller/default.js to match the above "/" route.
+It must reside in ./controller/_controllerName_.js - for example ./controller/default.js to match the above "/" route.
 
 It need to have the following structure:
 
@@ -66,27 +69,67 @@ It need to have the following structure:
 
     	// This is the data we send back to the callback
     	// It can be calls to other subcontrollers, database calls etc
-    	var data = {'foo': 'bar'};
+    	var data = {
+    		'head': {
+    			'title': 'foobar'
+    		},
+    		foo': 'bar'
+    	};
 
     	callback(null, request, response, data);
     }
+
+### Views
+
+A view is used to match the data from the controller with one or more templates. Views are by default positioned in ./public/views/_controllerName_.js and a typical default file can look like this:
+
+    'use strict';
+
+    var larvitviews = require('larvitviews')();
+
+    exports.run = function(data, callback) {
+    	var partials;
+
+    	partials = [
+    		{
+    			'partName': 'aboveBody',
+    			'tmplName': 'head',
+    			'data': data.head
+    		},
+    		{
+    			'tmplName': 'default',
+    			'data': data
+    		}
+    	];
+
+    	data.tmplParts = {}; // This will contain the different template parts
+    	                     // that will be stitched together for a complete page
+
+    	larvitviews.renderPartials(partials, data, function(err, returnStr) {
+    		callback(err, returnStr);
+    	});
+    };
 
 ### Templates
 
 Larvitbase use [underscore templates](http://underscorejs.org/#template). They are fed with the data sent in the callback from the controller.
 
-The templates resides in ./public/tmpl/<controllerName>.tmpl - for example ./public/tmpl/default.tmpl to match the above "/" route.
+The templates resides in ./public/views/tmpl/_controllerName_.tmpl - for example ./public/viewstmpl/default.tmpl to match the above "/" route.
 
 An example template can look like:
 
     <!DOCTYPE html>
     <html lang="en">
-    	<head>
-    		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-    		<title>larvit.se</title>
-    	</head>
+    	<%= tmplParts['aboveBody'] %>
     	<body>
     		<h1>Look at our beautiful data below</h1>
     		<p><%= foo %></p>
     	</body>
     </html>
+
+The example head section, ./public/views/tmpl/head.tmpl, can look like this:
+
+    <head>
+    	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    	<title><%= title %></title>
+    </head>
--- base.js
+++ base.js
@@ -4,27 +4,32 @@ var path        = require('path'),
     appPath     = path.dirname(require.main.filename),
     serverConf  = require(appPath + '/config/server.json'),
     http        = require('http'),
-    serveStatic = require('serve-static')(serverConf.pubFilePath, {'index': false}),
+    serveStatic = require('serve-static')(serverConf.pubFilePath, {'index': false, 'maxAge': 1000}),
     formidable  = require('formidable'),
-    router      = undefined;
+    log         = require('winston'),
+    cuid        = require('cuid'),
+    merge       = require('utils-merge'),
+    utils       = require('larvitutils'),
+    router;
 
-createServer(require(appPath + '/config/routes.json'));
+function executeController(request, response) {
+	function sendToClient(err, request, response, data) {
+		router.sendToClient(err, request, response, data, function() {
+			var timer = utils.hrtimeToMs(request.startTime);
 
-// Actually create the server
-function createServer(customRoutes) {
-	router = require('larvitrouter')({'customRoutes': customRoutes});
-
-	http.createServer(function(request, response) {
-		router.resolve(request, function(err){
-			if (err) {
-				console.error(err);
-				request.controllerName = '404';
-			}
-
-			// We need to parse the request a bit for POST values etc before we hand it over to the controller(s)
-			parseRequest(request, response);
+			log.debug('larvitbase: Request #' + request.cuid + ' complete in ' + timer + 'ms');
 		});
-	}).listen(serverConf.port, '127.0.0.1');
+	}
+
+	if (request.staticFilename !== undefined) {
+		log.debug('larvitbase: Serving static file: ' + request.staticFilename);
+
+		serveStatic(request, response);
+	} else if (request.controllerName !== undefined) {
+		require(appPath + '/controllers/' + request.controllerName).run(request, response, sendToClient);
+	} else {
+		require(appPath + '/controllers/404').run(request, response, sendToClient);
+	}
 }
 
 /**
@@ -32,12 +37,14 @@ function createServer(customRoutes) {
  * Fetch POST data etc
  */
 function parseRequest(request, response) {
-	if (request.method == 'POST') {
-		var form = new formidable.IncomingForm();
+	var form;
+
+	if (request.method === 'POST') {
+		form = new formidable.IncomingForm();
 
 		form.parse(request, function(err, fields, files){
 			if (err) {
-				console.error(err);
+				log.error(err.message, err);
 				response.writeHead(500, {'content-type': 'text/plain'});
 				response.end('Internal server error');
 			} else {
@@ -52,11 +59,33 @@ function parseRequest(request, response) {
 	}
 }
 
-function executeController(request, response) {
-	if (request.staticFilename !== undefined)
-		serveStatic(request, response);
-	else if (request.controllerName !== undefined)
-		require(appPath + '/controllers/' + request.controllerName).run(request, response, router.sendToClient);
-	else
-		require(appPath + '/controllers/404').run(request, response, router.sendToClient);
-}
+exports = module.exports = function(options) {
+	log.info('larvitbase: Creating server on ' + serverConf.host + ':' + serverConf.port);
+
+	// Set default options
+	options = merge({
+		'pubFilePath':  serverConf.pubFilePath,
+		'tmplDir':      'tmpl',
+		'port':         serverConf.port,
+		'host':         serverConf.host,
+		'customRoutes': []
+	}, options);
+
+	router = require('larvitrouter')({'customRoutes': options.customRoutes});
+
+	http.createServer(function(request, response) {
+		request.cuid      = cuid();
+		request.startTime = process.hrtime();
+		log.debug('larvitbase: Starting request #' + request.cuid + ' to: "' + request.url);
+
+		router.resolve(request, function(err) {
+			if (err) {
+				// Could not be resolved, this is logged in router.reslove()
+				request.controllerName = '404';
+			}
+
+			// We need to parse the request a bit for POST values etc before we hand it over to the controller(s)
+			parseRequest(request, response);
+		});
+	}).listen(options.port, options.host);
+};
